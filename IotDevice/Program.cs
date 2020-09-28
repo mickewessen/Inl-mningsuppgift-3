41b2492b6e6a0243413a1a507b80031d2652bdfa
Micke Wessén <micke.wessen@hotmail.com>
2020-09-28 12:45:39 +0200

    Add project files.

---
+++ IotDevice/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System;
+
+namespace IotDevice
+{
+    class Program
+    {
+        private static readonly string _conn = "HostName=ec-win-iothub.azure-devices.net;DeviceId=ConsoleApp;SharedAccessKey=3zsfBvJy2yTIaBEoTQpnnbD1jX4Z1Juz3g8jhLSjX8M=";
+
+
+
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+
+        static void Main(string[] args)
+        {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceService.RecieveMessageAsync(deviceClient).GetAwaiter();
+
+            Console.ReadKey();
+        }
+    }
+}
