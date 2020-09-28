b4a70c11ae337c0a0f62574f18e6d7b50dc8aead
Micke Wessén <micke.wessen@hotmail.com>
2020-09-28 14:26:39 +0200

    1.0

--- IotDevice/Program.cs
+++ IotDevice/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Azure.Devices.Client;
+using SharedLibraries.Services;
 using System;
+using System.Threading.Tasks;
 
 namespace IotDevice
 {
@@ -7,15 +9,14 @@ namespace IotDevice
     {
         private static readonly string _conn = "HostName=ec-win-iothub.azure-devices.net;DeviceId=ConsoleApp;SharedAccessKey=3zsfBvJy2yTIaBEoTQpnnbD1jX4Z1Juz3g8jhLSjX8M=";
 
-
-
         private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
 
 
         static void Main(string[] args)
         {
-            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
-            DeviceService.RecieveMessageAsync(deviceClient).GetAwaiter();
+
+            DeviceServices.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceServices.RecieveMessageAsync(deviceClient).GetAwaiter();
 
             Console.ReadKey();
         }
